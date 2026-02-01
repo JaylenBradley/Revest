d054be1cb58135c95b2244f95c5dffc995258022
JayBradCoding <jbradl24@villanova.edu>
2026-01-31 23:46:27 -0500

    Updated alembic imports and db connection

--- backend/app/core/firebase.py
+++ backend/app/core/firebase.py
@@ -1,5 +1,12 @@
+import os
+from .config import settings
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+if os.path.exists("/etc/secrets/firebase-adminsdk.json"):
+    firebase_cred_path = "/etc/secrets/firebase-adminsdk.json"
+else:
+    firebase_cred_path = settings.firebase_cred_path
+
+cred = credentials.Certificate(firebase_cred_path)
 firebase_admin.initialize_app(cred)
